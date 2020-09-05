55e397998100505d1d5df526b8ca4a24c1074398
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2020-09-05 08:58:33 +0530

    add useState hook in baseConverter

--- src/BaseConvertor.js
+++ src/BaseConvertor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from './Input';
 
 const isValidNumberForBase = function (value, base) {
@@ -7,37 +7,26 @@ const isValidNumberForBase = function (value, base) {
     .every((digit) => Number.isInteger(parseInt(digit, base)));
 };
 
-class BaseConvertor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { number: 0 };
-    this.onChange = this.onChange.bind(this);
-  }
+const BaseConvertor = (props) => {
+  const [state, setState] = useState({ number: 0 });
 
-  onChange({ value }, baseId) {
+  const onChange = ({ value }, baseId) => {
     if (isValidNumberForBase(value, baseId)) {
-      this.setState({ number: parseInt(value, baseId) });
+      setState({ number: parseInt(value, baseId) });
     }
-  }
+  };
 
-  render() {
-    const baseIds = [...Array(this.props.count - 1).keys()];
-    const inputSections = baseIds.map((id) => (
-      <Input
-        key={id}
-        baseId={id + 2}
-        number={this.state.number}
-        onChange={this.onChange}
-      />
-    ));
+  const baseIds = [...Array(props.count - 1).keys()];
+  const inputSections = baseIds.map((id) => (
+    <Input key={id} baseId={id + 2} number={state.number} onChange={onChange} />
+  ));
 
-    return (
-      <div className="container">
-        <h1>BaseConvertor</h1>
-        {inputSections}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container">
+      <h1>BaseConvertor</h1>
+      {inputSections}
+    </div>
+  );
+};
 
 export default BaseConvertor;
